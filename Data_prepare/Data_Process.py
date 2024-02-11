aebf662281174eda96cea1c2586cc635d9547824
Yunbo Long <82950147+Yunbo-max@users.noreply.github.com>
2024-02-11 12:34:03 +0000

    updates

--- Data_prepare/Data_Process.py
+++ Data_prepare/Data_Process.py
@@ -2,118 +2,59 @@
 # @Author: Yunbo
 # @Date:   2024-02-11 00:22:54
 # @Last Modified by:   Yunbo
-# @Last Modified time: 2024-02-11 12:13:12
+# @Last Modified time: 2024-02-11 12:31:39
 
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split,cross_val_score, cross_val_predict
-from sklearn.preprocessing import MinMaxScaler,StandardScaler
-from sklearn.linear_model import Ridge,LinearRegression,LogisticRegression,ElasticNet, Lasso
-from sklearn.ensemble import RandomForestRegressor,RandomForestClassifier, GradientBoostingRegressor,BaggingClassifier,ExtraTreesClassifier
-from sklearn.metrics import accuracy_score,mean_squared_error,recall_score,confusion_matrix,f1_score,roc_curve, auc
-from sklearn.metrics import mean_absolute_error, mean_squared_error
-import numpy as np
 from sklearn.model_selection import train_test_split
-import warnings
-from sklearn.preprocessing import LabelEncoder
-import matplotlib.pyplot as plt
-import seaborn as sns
-from tabulate import tabulate
+import torch
+from torch.utils.data import Dataset, DataLoader
+import pickle
 
+# Read the dataset
+dataset = pd.read_csv('./Data_collection/DataCoSupplyChainDataset.csv', encoding='latin-1')
 
-# Hiding the warnings
-warnings.filterwarnings('ignore')
-
-dataset =pd.read_csv('./Data/DataCoSupplyChainDataset.csv',encoding='latin-1')
+# Display options
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
-# Sort the DataFrame based on the "DateOrders" column
-df_sorted = dataset.sort_values(by='order date (DateOrders)')
+# Display the first 30 rows of the dataset
+print(dataset.head(30))
 
-# If you want to keep the original index order, you can reset the index
-df_sorted.reset_index(drop=True, inplace=True)
+# Define a PyTorch dataset class
+class CustomDataset(Dataset):
+    def __init__(self, data):
+        self.data = data
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        # Implement your own data retrieval logic here
+        # For example, you can return a tuple of features and labels
+        return self.data.iloc[idx, :-1], self.data.iloc[idx, -1]
 
-df_sorted['order date (DateOrders)'] = pd.to_datetime(df_sorted['order date (DateOrders)'])
-df_sorted['shipping date (DateOrders)'] = pd.to_datetime(df_sorted['shipping date (DateOrders)'])
-
-# Create a new column 'operation time' representing the difference between order date and shipping date
-df_sorted['operation time'] = df_sorted['shipping date (DateOrders)'] - df_sorted['order date (DateOrders)']
-
-# Set the 'DateOrders' column as the index
-df_sorted.set_index('order date (DateOrders)', inplace=True)
-
-df_sorted=df_sorted.drop(columns=['shipping date (DateOrders)'])
-
-
-
-dataset['Customer Full Name'] = dataset['Customer Fname'] + dataset['Customer Lname']
-dataset['TotalPrice'] = dataset['Order Item Quantity'] * dataset[
-    'Sales per customer']  # Multiplying item price * Order quantity
-
-data = dataset.drop(
-    ['Customer Email', 'Customer Id', 'Customer Password', 'Customer Fname', 'Customer Lname',
-      'Product Description', 'Product Image', 'Order Zipcode','Product Status','Order Profit Per Order','Product Price'], axis=1)
-
-data['Customer Zipcode'] = data['Customer Zipcode'].fillna(0)  # Filling NaN columns with zero
-
-
-data['order_year'] = pd.DatetimeIndex(data['order date (DateOrders)']).year
-data['order_month'] = pd.DatetimeIndex(data['order date (DateOrders)']).month
-data['order_week_day'] = pd.DatetimeIndex(data['order date (DateOrders)']).day_name()
-data['order_hour'] = pd.DatetimeIndex(data['order date (DateOrders)']).hour
-# data['order_second'] = pd.DatetimeIndex(data['order date (DateOrders)'])
-
-data['shipping_year'] = pd.DatetimeIndex(data['shipping date (DateOrders)']).year
-data['shipping_month'] = pd.DatetimeIndex(data['shipping date (DateOrders)']).month
-data['shipping_week_day'] = pd.DatetimeIndex(data['shipping date (DateOrders)']).day_name()
-data['shipping_hour'] = pd.DatetimeIndex(data['shipping date (DateOrders)']).hour
-
-label_data = data[['shipping_week_day','order_week_day','Customer Full Name','Type','Delivery Status','Category Name','Customer City','Customer Country','Customer Segment','Customer State','Customer Street','Department Name','Market','Order City','Order Country','order date (DateOrders)','Order State','Order Status','Product Name','shipping date (DateOrders)','Shipping Mode']]
+# Split the dataset according to 'Order Region' and save into PyTorch datasets
+datasets = {}
+for region, data_region in dataset.groupby('Order Region'):
+    # Split data into features and labels
+    features = data_region.drop(columns=['Order Region'])
+    labels = data_region['Order Region']
+    
+    # Split into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+    
+    # Create PyTorch datasets
+    train_dataset = CustomDataset(pd.concat([X_train, y_train], axis=1))
+    test_dataset = CustomDataset(pd.concat([X_test, y_test], axis=1))
+    
+    # Store datasets
+    datasets[region] = {'train': train_dataset, 'test': test_dataset}
 
 
-target=data['Sales']
-
-data=data.drop(columns=['shipping_week_day','order_week_day','Customer Full Name','Sales','Type','Delivery Status','Category Name','Customer City','Customer Country','Customer Segment','Customer State','Customer Street','Department Name','Market','Order City','Order Country','order date (DateOrders)','Order Region','Order State','Order Status','Product Name','shipping date (DateOrders)','Shipping Mode'])
-
-
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
-print(data.head(30))
+# Save datasets to disk
+with open('datasets.pkl', 'wb') as f:
+    pickle.dump(datasets, f)
 
 
-
-
-
-# dataset = pd.read_csv('./Data/DataCoSupplyChainDataset.csv', encoding='latin1')
-
-# markets = dataset['index'].unique()
-# region_mapping = {region: i for i, region in enumerate(markets)}
-
-
-# dataset['Region Index'] = dataset['index'].map(region_mapping)
-
-# dataset = dataset.drop(columns=['index'])
-
-
-# print(dataset)
-# print(region_mapping)
-
-
-# markets = dataset['Region Index'].unique()
-
-# print(markets)
-
-
-# with h5py.File('../Data/market_data.h5', 'w') as f:
-#     for market in markets:
-#         market_str = str(market)
-#         market_data = dataset[dataset['Region Index'] == market]
-#         print(market_str)
-#         print(market_data)
-#         # Store the data as a dataset
-#         f.create_dataset(market_str, data=market_data.to_numpy())
-#         # Store the column names as an attribute
-#         f[market_str].attrs['columns'] = market_data.columns.tolist()
---
+++ Data_processing/processing.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Author: Yunbo
+# @Date:   2024-02-11 12:23:32
+# @Last Modified by:   Yunbo
+# @Last Modified time: 2024-02-11 12:23:32
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
