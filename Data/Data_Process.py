f283eb584d8b764eb70f9e5c9157f421c4453654
Yunbo Long <82950147+Yunbo-max@users.noreply.github.com>
2024-02-11 11:35:21 +0000

    updates

---
+++ Data/Data_Process.py
@@ -0,0 +1,50 @@
+# -*- coding = utf-8 -*-
+# @time:08/07/2023 08:12
+# Author:Yunbo Long
+# @File:split.py
+# @Software:PyCharm
+# -*- coding = utf-8 -*-
+# @time:08/07/2023 01:50
+# Author:Yunbo Long
+# @File:split.py
+# @Software:PyCharm
+
+import pandas as pd
+import h5py
+
+import pandas as pd
+
+# Read the integrated_train_data.csv file
+dataset = pd.read_csv('E:\Federated_learning_flower\experiments\Presentation\integrated_train_data_ISMM.csv')
+
+# Get unique market values
+markets = dataset['index'].unique()
+
+# Create a dictionary to map regions to numbers
+region_mapping = {region: i for i, region in enumerate(markets)}
+
+# Add a new column 'Region Index' to represent the regions with numbers
+dataset['Region Index'] = dataset['index'].map(region_mapping)
+
+dataset = dataset.drop(columns=['index'])
+
+# Print the modified dataset
+print(dataset)
+print(region_mapping)
+
+# Get unique market values
+markets = dataset['Region Index'].unique()
+
+print(markets)
+
+#
+with h5py.File('market_data.h5', 'w') as f:
+    for market in markets:
+        market_str = str(market)
+        market_data = dataset[dataset['Region Index'] == market]
+        print(market_str)
+        print(market_data)
+        # Store the data as a dataset
+        f.create_dataset(market_str, data=market_data.to_numpy())
+        # Store the column names as an attribute
+        f[market_str].attrs['columns'] = market_data.columns.tolist()
